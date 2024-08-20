4684c1e0d1de5daafaeed9709f7ab0c783c91fe5
Hashemdl <hashemdalijeh@gmail.com>
2024-08-20 18:52:13 +0330

    🟡 Add List Sessions

--- pyutilities/functools.py
+++ pyutilities/functools.py
@@ -1,5 +1,12 @@
+from pathlib import Path
+
 
 def chunk_generate(input_list, chunk_size):
     '''List Chunk Generate'''
     for i in range(0, len(input_list), chunk_size):
         yield input_list[i:i + chunk_size]
+
+def get_sessions(path: str | Path) -> list[Path]:
+    _path = path if isinstance(path, Path) else Path(path)
+    sessions = [x for x in _path.iterdir() if x.suffix == '.session']
+    return sessions
