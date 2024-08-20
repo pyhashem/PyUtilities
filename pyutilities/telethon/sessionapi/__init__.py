ecf3ca72ec684cd36a0f078e3b6c4b257de337e2
Hashemdl <hashemdalijeh@gmail.com>
2024-08-20 16:11:49 +0330

    fix import init

--- pyutilities/telethon/sessionapi/__init__.py
+++ pyutilities/telethon/sessionapi/__init__.py
@@ -1,3 +1,3 @@
 from pyutilities.telethon.sessionapi.session import SESSION
-from pyutilities.telethon.sessionapi.apis.api import API
-from pyutilities.telethon.sessionapi.apis.api import APIData
+from pyutilities.telethon.sessionapi.apis import API
+from pyutilities.telethon.sessionapi.apis import APIData
---
+++ pyutilities/telethon/sessionapi/apis/__init__.py
@@ -0,0 +1,2 @@
+from pyutilities.telethon.sessionapi.apis.api import API
+from pyutilities.telethon.sessionapi.apis.api import APIData
