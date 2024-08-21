efd77e93d29b6ef0fa9c2fff9b31e95884d59061
Hashemdl <hashemdalijeh@gmail.com>
2024-08-21 14:05:34 +0330

    change name extras async to asyncio

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Python Utily tools'
 LONG_DESCRIPTION = 'python common utility tools'
 
@@ -27,7 +27,7 @@ setup(
             'python_socks',
             'async-timeout',
         ],
-        "async" : [
+        "asyncio" : [
             'aiofiles'
         ]
     },
